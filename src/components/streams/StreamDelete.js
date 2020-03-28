65eb5f4615a2701dce37f8e34ad70c8b509bdfcf
Varun Mehta <mehtav23@gmail.com>
2020-03-28 15:48:09 +0530

    added modal as portal and edit and delete functionality

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
 
+import createBroswerHistory from '../../history';
 
-const StreamDelete = () => {
-  return <div>Stream Delete</div>  
+import { fetchStream, deleteStream } from '../../actions';
+
+class StreamDelete extends React.Component {
+
+  componentDidMount()  {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  renderActions = () =>{
+    return (
+      <React.Fragment>
+        <button className="ui button negative"
+          onClick={this.onDelete}>Delete</button>
+        <Link to="/"
+        className="ui button">
+        Cancel</Link>
+      </React.Fragment>
+    );  
+  };
+
+  onDelete = () => {
+    this.props.deleteStream(this.props.match.params.id);
+  }
+  
+  renderContent() {
+    if(!this.props.stream) {
+      return 'Are you sure you want to delete this stream?'
+    }
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+  }
+
+  render() {
+    return (
+     <Modal
+          title="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={() => createBroswerHistory.push('/')}
+        />
+    );
+  };
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream : state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, {
+  fetchStream, deleteStream
+})(StreamDelete);
